678e627ede1f17417bc1bb9f6bc8d22551a4256b
Rosario Borgesi <rosario.borgesi@larus-ba.com>
2023-06-21 17:44:07 +0200

    commit producer

---
+++ producer/src/main/java/rosariob/kafka/VehiclePositionProducer.java
@@ -0,0 +1,34 @@
+package rosariob.kafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.eclipse.paho.client.mqttv3.*;
+import org.slf4j.Logger;
+import org.slf4j.reload4j.Reload4jLoggerFactory;
+import org.slf4j.reload4j.Reload4jServiceProvider;
+
+public class VehiclePositionProducer {
+
+    public static void main(String[] args) throws MqttException {
+        System.out.println("*** Starting VP Producer ***");
+
+        Properties settings = new Properties();
+        settings.put(ProducerConfig.CLIENT_ID_CONFIG, "vp-producer");
+        settings.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        settings.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        settings.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        final KafkaProducer<String, String> producer = new KafkaProducer<>(settings);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("### Stopping VP Producer ###");
+            producer.close();
+        }));
+
+        Subscriber subscriber = new Subscriber(producer);
+        subscriber.start();
+    }
+}
